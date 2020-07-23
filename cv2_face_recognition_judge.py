9c9ba17e2bdb9faeebf679b5fbaf7bfda6455960
data2 <muskteerathos@gmail.com>
2020-07-23 10:09:30 +0800

    人脸识别

--- cv2_face_recognition_judge.py
+++ cv2_face_recognition_judge.py
@@ -15,21 +15,21 @@ import numpy as np
 video_capture = cv2.VideoCapture(0)
 
 # Load a sample picture and learn how to recognize it.
-obama_image = face_recognition.load_image_file("obama.jpg")
+obama_image = face_recognition.load_image_file("/Users/leewow/code/github/data2/face-looker/source/abm.jpeg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
 # Load a second sample picture and learn how to recognize it.
-biden_image = face_recognition.load_image_file("biden.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+trump_image = face_recognition.load_image_file("/Users/leewow/code/github/data2/face-looker/source/tlp.jpeg")
+trump_face_encoding = face_recognition.face_encodings(trump_image)[0]
 
 # Create arrays of known face encodings and their names
 known_face_encodings = [
     obama_face_encoding,
-    biden_face_encoding
+    trump_face_encoding
 ]
 known_face_names = [
     "Barack Obama",
-    "Joe Biden"
+    "Donald John Trump"
 ]
 
 # Initialize some variables
